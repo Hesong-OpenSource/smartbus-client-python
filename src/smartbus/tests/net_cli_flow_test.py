92540db09c5a2334b016dec3bfa14204c2770359
lxy <lxy@hesong.net>
2013-12-02 16:48:18 +0800

    增加了一个测试 net flow invoke 测试用例

--- src/smartbus/tests/net_cli_flow_test.py
+++ src/smartbus/tests/net_cli_flow_test.py
@@ -7,6 +7,11 @@ Created on 2013-3-5
 author: tanbro
 '''
 import sys
+import logging
+
+logging.root.addHandler(logging.StreamHandler())
+logging.root.setLevel(logging.DEBUG)
+
 
 if sys.version_info[0] == 2:
     readln = raw_input
@@ -15,6 +20,7 @@ elif sys.version_info[0] == 3:
     
 if __name__ == '__main__':
     import smartbus.netclient
+    import smartbus.netclient._c_smartbus_netcli_interface
     
     def onReceiveText(self, packInfo, txt):
         '''
@@ -25,23 +31,23 @@ if __name__ == '__main__':
         print(txt)
 
     def flow_response(pack_info, project, invoke_id, result):
-        print(invoke_id, result)
+        print('response:', invoke_id, result)
 
     def flow_timeout(pack_info, project, invoke_id):
-        print(invoke_id, timeout)
+        print('timeout:', invoke_id)
 
     def connect_succeed(unit_id):
-        print('smartbus connect_succeed: %s'%(unit_id))
+        print('smartbus connect_succeed: %s' % (unit_id))
 
     def connect_failed(unit_id, errno):
-        print('smartbus connect_failed: %s, %s'%(unit_id, errno))
+        print('smartbus connect_failed: %s, %s' % (unit_id, errno))
 
     def disconnected():
         print('disconnected')
     
-    #smartbus.netclient.Client.initialize(26, '''/media/08CAB087CAB07314/My Projects/TK ClientsServiceSystem/smartbus/lib/linux-gcc4.1.2-x86/libbusnetcli.so''')
-    smartbus.netclient.Client.initialize(14, '''E:\My Projects\TK ClientsServiceSystem\smartbus\lib\windows-msc1600-x86\smartbus_net_cli.dll''')
-    cli = smartbus.netclient.Client(localClientId=0, localClientType=-1, masterHost='192.168.3.20', masterPort=8089, encoding='utf-8')
+    smartbus.netclient.Client.initialize(26)
+    print('library file = {}'.format(smartbus.netclient._c_smartbus_netcli_interface._lib))
+    cli = smartbus.netclient.Client(localClientId=0, localClientType=25, masterHost='10.4.62.45', masterPort=8089, encoding='utf-8')
 
     cli.onConnectSuccess = connect_succeed
     cli.onConnectFail = connect_failed
@@ -55,5 +61,5 @@ if __name__ == '__main__':
 
     while True:
         s = readln()
-        #cli.sendText(cmd=1, cmdType=1, dstUnitId=0, dstClientId=15, dstClientType=15, txt=s)
-        cli.invokeFlow(0, 0, 'Project1', 'WebChatRpc', {'method':'test', 'params':[]}, True, 5)
+        # cli.sendText(cmd=1, cmdType=1, dstUnitId=0, dstClientId=15, dstClientType=15, txt=s)
+        cli.invokeFlow(0, 0, 'Project2', '_webchat_rpc', {'method':'RobotConnected', 'params':['robot-001']}, True, 5)
---
+++ src/smartbus/tests/test_webchatrpc.py
@@ -0,0 +1,111 @@
+'''
+Created on 2013年12月2日
+
+@author: tanbro
+'''
+import unittest
+import logging
+import threading
+
+from smartbus.netclient import Client
+from smartbus.netclient import _c_smartbus_netcli_interface as clib
+
+logging.root.addHandler(logging.StreamHandler())
+logging.root.level = logging.DEBUG
+
+jsonrpc_version = '2.0'
+
+CMDTYPE_JSONRPC_REQ = 211
+'''JSONRPC 请求. 用于 smartbus 客户端 send 函数的 cmd_type 参数.'''
+
+CMDTYPE_JSONRPC_RES = 212
+'''JSONRPC 回复. 用于 smartbus 客户端 send 函数的 cmd_type 参数.'''
+
+
+class Test(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+        cond_connect = threading.Condition()
+        
+        cls._recv_buf = {}
+        cls._recv_buf_lock = threading.Lock()
+        
+        def onReceiveText(self, packInfo, txt):
+            print(txt)
+
+        pass
+    
+        def flow_response(packInfo, project, invokeId, result):
+            print('response:', invokeId, result)
+            with cls._recv_buf_lock:
+                pending = cls._recv_buf[invokeId]
+            cond = pending[0]
+            cond.acquire()
+            pending[1] = result
+            cond.notify()
+            cond.release()
+            
+        pass
+    
+        def flow_timeout(packInfo, project, invokeId):
+            print('timeout:', invokeId)
+        pass
+    
+        def connect_succeed(unit_id):
+            print('smartbus connect_succeed: %s' % (unit_id))
+            cls._connected = True
+            cond_connect.acquire()
+            cond_connect.notify()
+            cond_connect.release()
+        pass
+    
+        def connect_failed(unit_id, errno):
+            print('smartbus connect_failed: %s, %s' % (unit_id, errno))
+            cls._connected = False
+            cond_connect.acquire()
+            cond_connect.notify()
+            cond_connect.release()
+        pass
+    
+        def disconnected():
+            print('disconnected')
+            cls._connected = False
+        pass
+        
+        Client.initialize(26)
+        print('library file = {}'.format(clib._lib))
+        _cli = cls._cli = Client(localClientId=0, localClientType=25, masterHost='10.4.62.45', masterPort=8089, encoding='utf-8')
+    
+        _cli.onConnectSuccess = connect_succeed
+        _cli.onConnectFail = connect_failed
+        _cli.onDisconnect = disconnected
+        _cli.onInvokeFlowRespond = flow_response
+        _cli.onInvokeFlowTimeout = flow_timeout
+        _cli.onReceiveText = onReceiveText
+        
+        print('conneting...')
+        cond_connect.acquire()
+        _cli.connect()
+        cond_connect.wait()
+        assert cls._connected
+
+
+    def test_robot_connected(self):
+        cond = threading.Condition()
+        cond.acquire()
+        with self._recv_buf_lock:
+            flowid = self._cli.invokeFlow(0, 0, 'Project2', '_webchat_rpc', {'method':'RobotConnected', 'params':['robot-001']}, True, 5)
+            pending = [cond, None]
+            self._recv_buf[flowid] = pending
+        cond.wait()
+        retval = pending[1]
+        print(retval)
+        retval = retval[0]
+        self.assertIn('result', retval)
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.test_robot_connected']
+    unittest.main()
